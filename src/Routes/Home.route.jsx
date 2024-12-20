34f2fa6ef4ee8f66a4acfb25f1244cd81e918e82
Yuvraj <yuvrajbhinchar00@gmail.com>
2024-12-20 11:26:17 +0300

    Home Componants basic develope

--- src/Routes/Home.route.jsx
+++ src/Routes/Home.route.jsx
@@ -1,58 +1,48 @@
-function Home(){
-
-    return(
-        <>
-            {/* Main Div */}
-            <div>
-                {/* 1 */}
-                <div>
-                    {/* A */}
-                    <div>
-                        {/* Img */}
-                        <div></div>
-                        {/* Intro */}
-                        <div>
-                            {/* Status */}
-                            <div>Available To Work</div>
-                            <h1>Yuvraj Bhinchar</h1>
-                            <h2>Jaipur,India</h2>
-                        </div>
-                    </div>
-                    {/* B */}
-                    <div>
-                        <div>
-                        <div>FullStack Developer</div>
-                        <div>React</div>
-                        <div>Spring Boot</div>
-                        </div>
-                        <div>
-                        👋Hi, I’m Yuvraj, a Full Stack Developer skilled in building scalable web applications. I have hands-on experience with modern frameworks and have worked on projects like interactive web apps and optimized backend systems. Always eager to learn and create impactful solutions.
-                        </div>
-                    </div>
-                    {/* C */}
-                    <div>
-                        <div>
-                            <div>Certificates</div>
-                            <div>Resume</div>
-                        </div>
-                        <div>👉 Let’s Build Something Great Together – Stay Connected
-                        </div>
-                    </div>
-                    {/* D */}
-                    <div>
-                        <div>GitHub</div>
-                        <div>X</div>
-                        <div>Linkdl</div>
-                        <div>Insta</div>
-                    </div>
-                </div>
-                 {/* 2 */}
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
+import profile from "../assets/profile.jpg";
+import { Link } from "react-router";
+function Home() {
+  return (
+    <>
+      {/* main div */}
+      <div className="bg-black w-full h-screen text-white">
+        {/* 1 */}
+        <div className="flex flex-row-reverse w-full pt-6">
+          {/* img */}
+          <div className="w-1/2 flex justify-center items-center">
+            <div className="w-96 h-96 rounded-full overflow-hidden">
+              <img src={profile} alt="Profile" className="w-full h-full object-top" />
+            </div>             
+          </div>
+          {/* Intro */}
+          <div className="w-1/2">
+            <h2 className="mt-10 ml-10 text-lg">Welcome To My World</h2>
+            <h1 className="ml-10 text-4xl text-green-400 my-6">Hi, I'm Yuvraj Bhinchar</h1>
+            <div className="ml-10">
+              I'm a Full Stack Developer skilled in building scalable web applications. I have hands-on experience with modern frameworks and have worked on projects like interactive web apps and optimized backend systems. Always eager to learn and create impactful solutions.
             </div>
-        </>
-    )
+          </div>
+        </div>
+
+        {/* 2 */}
+        <div>
+          {/* A */}
+          <div className="ml-10">
+            <h1>👉 Let’s Build Something Great Together – Stay Connected</h1>
+            <div className="flex">
+              <div className="mx-2">
+                <Link to="/t">Github</Link>
+              </div>
+              <div className="mx-2"> <Link to="/t">Linkdl</Link></div>
+              <div className="mx-2"> <Link to="/t">Whatsapp</Link></div>
+              <div className="mx-2"> <Link to="/t">x</Link></div>
+            </div>
+          </div>
+          {/* B */}
+          <div></div>
+        </div>
+      </div> 
+    </>
+  );
 }
+
 export default Home;
--- src/componantes/Navbar.jsx
+++ src/componantes/Navbar.jsx
@@ -1,18 +1,34 @@
+import { Link } from "react-router";
 function Navbar(){
 
     return(
         <>
             {/* Main Div */}
-            <div className="w-full h-24 bg-black">
-                <div className="flex justify-evenly pt-8">
-                <div className="text-white font-extrabold">
-                    Home
+            <div className="bg-black w-full h-20">
+                <div className="flex justify-around pt-7">
+                <div className="text-white font-extrabold">Yuvi</div>
+                <div className="flex justify-between">
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/" >Home</Link>
                 </div>
-                <div className="text-white font-extrabold">
-                    Work
+                <div className="text-white font-extrabold mr-5">
+                <Link to="/srvice" >Service</Link>
                 </div>
-                <div className="text-white font-extrabold">
-                    Resume
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/resume">Resume</Link>
+                </div>
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/work">Work</Link>
+                </div>
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/contact">contact</Link>
+                </div>
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/abouteme">About Me</Link>
+                </div>
+                <div className="text-white font-extrabold mr-5">
+                    <Link to="/blog">Blog</Link>
+                </div>
                 </div>
                 </div>
             </div>
